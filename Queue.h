a4847e7fb9de7b6288f051e583785355f39ce23e
yaseenallan <157521935+yaseenallan@users.noreply.github.com>
2025-04-22 18:58:53 -0500

    Add files via upload

---
+++ Queue.h
@@ -0,0 +1,34 @@
+#pragma once
+#include <iostream>
+#include <vector>
+
+// Template Queue using linked list
+template <typename T>
+class Queue {
+private:
+    struct Node {
+        T data;
+        Node* next;
+        Node(T val) : data(val), next(nullptr) {}
+    };
+    Node* front_ptr;
+    Node* rear_ptr;
+    size_t count;
+
+public:
+    Queue();
+    ~Queue();
+
+    void push(const T& val);
+    void pop();
+    T front() const;
+    size_t size() const;
+    bool empty() const;
+    void move_to_rear();
+};
+
+// Recursive function to find last occurrence of a target in a vector
+int recursive_last_occurrence(const std::vector<int>& vec, int target, int index);
+
+// Insertion sort for a vector
+void insertion_sort(std::vector<int>& list);
